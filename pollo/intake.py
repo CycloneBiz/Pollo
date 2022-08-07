8934377f11b74bbee175bccd81009672cbf8212d
Owen Shaule <owen.shaule@gmail.com>
2022-08-07 16:42:04 -0400

    Intake mechanism

---
+++ pollo/intake.py
@@ -0,0 +1,12 @@
+def intake(func, *args, **kwargs):
+    def newfunc():
+        return func(*args, **kwargs)
+
+    return newfunc
+
+if __name__ == "__main__":
+    def my_annoying_func(message):
+        print(message)
+
+    x = intake(my_annoying_func, "hello world")
+    print(x())
